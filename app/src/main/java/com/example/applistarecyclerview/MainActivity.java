ac2bd8caa402b11c16e7f7e75eeb8e89878b03f1
NehuenCuenca <nehuen.cuenca@hotmail.com>
2020-06-26 20:13:23 -0300

    primer commit

---
+++ app/src/main/java/com/example/applistarecyclerview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.applistarecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> listDatos;
+    RecyclerView recycler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recycler=  (RecyclerView) findViewById(R.id.recyclerId);
+        recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
+        listDatos=new ArrayList<String>();
+        for (int i=0; i<=50; i++) {
+            listDatos.add("Dato # "+i+" ");
+        }
+
+        AdapterDatos adapter=new AdapterDatos(listDatos);
+        recycler.setAdapter(adapter);
+
+    }
+}
